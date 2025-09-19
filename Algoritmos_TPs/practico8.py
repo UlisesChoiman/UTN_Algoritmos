346dfabe6bf799443a731312bf0d4531a51fb492
UlisesChoiman <choimanzambrana.ulises.epetn20@gmail.com>
2025-09-19 09:48:07 -0300

    Ej 3

--- Algoritmos_TPs/practico8.py
+++ Algoritmos_TPs/practico8.py
@@ -28,3 +28,22 @@ while pila:
     pila_invertida.append(pila.pop())
 
 print("Pila invertida:", pila_invertida) 
+
+
+#3
+
+pila = []
+pila_invertida = []
+
+palabra = input("Ingrese una palabra para verificar si es palíndromo: ")
+for letra in palabra:
+    pila.append(letra)
+
+temp_pila = pila.copy()
+while temp_pila:
+    pila_invertida.append(temp_pila.pop())
+
+if pila == pila_invertida:
+    print(f"La palabra '{palabra}' es un palíndromo.")
+else:
+    print(f"La palabra '{palabra}' no es un palíndromo.")
