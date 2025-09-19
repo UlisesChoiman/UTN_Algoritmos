24b60847c3060896ac5fc7d388c4ae34ee3bee3a
UlisesChoiman <choimanzambrana.ulises.epetn20@gmail.com>
2025-09-19 09:37:24 -0300

    Ej 2

--- Algoritmos_TPs/practico8.py
+++ Algoritmos_TPs/practico8.py
@@ -13,3 +13,18 @@ while pila:
     pila_invertida.append(pila.pop())
 
 print("Pila invertida:", pila_invertida)
+
+
+#2
+pila = []
+pila.append("h")
+pila.append("o")
+pila.append("l")
+pila.append("a")
+print("Pila original:", pila)
+
+pila_invertida = [] 
+while pila: 
+    pila_invertida.append(pila.pop())
+
+print("Pila invertida:", pila_invertida) 
