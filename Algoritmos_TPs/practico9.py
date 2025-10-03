da162e983eac853f69ad0d2962e2ba0a6b48654e
UlisesChoiman <choimanzambrana.ulises.epetn20@gmail.com>
2025-10-03 09:30:34 -0300

    Ej 2

--- Algoritmos_TPs/practico9.py
+++ Algoritmos_TPs/practico9.py
@@ -22,3 +22,25 @@ nodo2.siguiente = nodo3
 nodo3.siguiente = nodo4
 
 print("Cantidad de nodos:", contar_nodos(nodo1)) 
+
+
+# Ej 2
+
+def encontrar_posicion(cabeza, valor_buscado):
+    posicion = 1
+    actual = cabeza
+    while actual is not None:
+        if actual.dato == valor_buscado:
+            return posicion
+        actual = actual.siguiente
+        posicion += 1
+    return -1
+
+nodox = int(input("Ingrese el valor del nodo a buscar: "))
+
+pos = encontrar_posicion(nodo1, nodox)
+if pos != -1:
+    print(f"El nodo con valor {nodox} está en la posición {pos}.")
+else:
+    print(f"El nodo con valor {nodox} no se encuentra en la lista.")
+
