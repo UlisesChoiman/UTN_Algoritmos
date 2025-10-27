9ad4f3ed2c5aba73c938d7b51209900b298cf49d
UlisesChoiman <choimanzambrana.ulises.epetn20@gmail.com>
2025-10-27 19:19:13 -0300

    Ej 3

--- Algoritmos_TPs/practico10.py
+++ Algoritmos_TPs/practico10.py
@@ -16,6 +16,7 @@ print("Lista original:", numbers)
 insertion_sort(numbers)
 print("Lista ordenada:", numbers)
 
+
 #2
 
 def selection_sort(arr):
@@ -32,3 +33,34 @@ print("\nLista original:", numbers2)
 
 selection_sort(numbers2)
 print("Lista ordenada:", numbers2)
+
+
+#3
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+numbers3 = []
+for i in range(6):
+    num = int(input(f"Ingrese el número {i+1}: "))
+    numbers3.append(num)
+
+numbers_insertion = numbers3.copy()
+numbers_selection = numbers3.copy()
+numbers_bubble = numbers3.copy()
+
+print("\nLista original:", numbers3)
+
+# Ordenar con los tres métodos
+insertion_sort(numbers_insertion)
+print("Ordenado por inserción:", numbers_insertion)
+
+selection_sort(numbers_selection)
+print("Ordenado por selección:", numbers_selection)
+
+bubble_sort(numbers_bubble)
+print("Ordenado por burbuja:", numbers_bubble)
