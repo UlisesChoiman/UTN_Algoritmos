6ec5447f0bcb2bfb46139afca079135695c56972
UlisesChoiman <choimanzambrana.ulises.epetn20@gmail.com>
2025-10-27 19:17:08 -0300

    Ej 2

--- Algoritmos_TPs/practico10.py
+++ Algoritmos_TPs/practico10.py
@@ -15,3 +15,20 @@ print("Lista original:", numbers)
 
 insertion_sort(numbers)
 print("Lista ordenada:", numbers)
+
+#2
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# Lista original
+numbers2 = [6, 2, 4, 8, 3, 7, 5]
+print("\nLista original:", numbers2)
+
+selection_sort(numbers2)
+print("Lista ordenada:", numbers2)
